f72d4d257047b4174e43c47b91bf147d1238ecb3
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2024-12-01 22:47:59 +0000

    Init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://0x15BA88FF.github.io',
+  base: 'ZxMaze',
+});
