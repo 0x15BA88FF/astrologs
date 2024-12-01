e234a2aa8322ebc2a8c8cf478b0f0701f215da94
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2024-12-01 23:15:03 +0000

    Clean up base project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://0x15BA88FF.github.io',
-  base: 'ZxMaze',
+    site: "https://0x15ba88ff.github.io",
+    base: "maze",
 });
