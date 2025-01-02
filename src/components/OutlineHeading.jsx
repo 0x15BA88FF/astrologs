466f92825475e71bf4986d35989d2f8152c72662
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-01-02 18:47:30 +0000

    Changed: resolve boolean logic condition

--- src/components/OutlineHeading.jsx
+++ src/components/OutlineHeading.jsx
@@ -9,7 +9,7 @@ const OutlineHeading = ({ heading, expanded }) => {
             <div className="flex items-center gap-2 w-full px-2 py-1 hover:bg-surface-700 rounded" style={{ paddingLeft: `${1 * heading.depth}rem` }}>
                 {heading.children?.length > 0? (
                     <button onClick={_ => setIsExpanded(!isExpanded)} className="flex items-center -ml-3">
-                        <ChevronRight size={20} className={ `transition-all duration-200 ${isExpanded && "rotate-90"}` } />
+                        <ChevronRight size={20} className={ `transition-all duration-200 ${isExpanded && "rotate-90" || ""}` } />
                     </button>
                 ) : (
                     <div className="mr-2" />
@@ -17,7 +17,7 @@ const OutlineHeading = ({ heading, expanded }) => {
                 <a href={`#${heading.slug}`} className="w-full text-sm">{heading.text}</a>
             </div>
             { heading.children?.length > 0 && (
-                <ul className={!isExpanded && "hidden"}>
+                <ul className={!isExpanded && "hidden" || ""}>
                     {heading.children.map(child => (
                         <OutlineHeading key={child.slug} heading={child} />
                     ))}
